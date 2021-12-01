8900fed5795060f060ac2440201271db48496e7e
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-12-01 13:12:32 -0800

    init

---
+++ Magnet Links/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Melanie Bilbrey on 12/1/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
