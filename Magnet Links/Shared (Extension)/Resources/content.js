a6c47d7d6d864b087990db2b3ffe5eb3c11a9cd6
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2021-12-01 20:15:43 -0800

    working content script

--- Magnet Links/Shared (Extension)/Resources/content.js
+++ Magnet Links/Shared (Extension)/Resources/content.js
@@ -1,7 +1,3 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+document.querySelectorAll('a[href^=magnet\\:]').forEach(el => {
+    el.href = `https://put.io/default/magnet?url=${el.href}`
+})
