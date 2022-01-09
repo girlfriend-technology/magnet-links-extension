e30cfb45e9add4333e35143fddba9c390573d0c8
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2022-01-08 16:39:37 -0800

    content script supports modes

--- content-script/src/index.js
+++ content-script/src/index.js
@@ -1,3 +1,18 @@
-document.querySelectorAll('a[href^=magnet\\:]').forEach(el => {
-  el.href = `https://put.io/default/magnet?url=${el.href}`
+import { getStorage } from './storage'
+import get from 'lodash/get'
+
+const DEFAULT_PREFIX = 'https://put.io/default/magnet?url='
+
+getStorage().then(({ data }) => {
+  document.querySelectorAll('a[href^=magnet\\:]').forEach(el => {
+    if (data.mode.shouldCopy) {
+      el.addEventListener('click', e => {
+        e.preventDefault()
+        navigator.clipboard.writeText(el.href)
+        window.alert('Copied to clipboard')
+      })
+    } else {
+      el.href = `${get(data, 'mode.prefix', DEFAULT_PREFIX)}${el.href}`
+    }
+  })
 })
